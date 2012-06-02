644f18f83ec658433dc5cc6e98400062d102d34c
Chad Gross <avatar4d@gmail.com>
2012-06-02 00:28:00 -0400

    Revert "Log rotate failed, added full path to mv"
    Did not include the listed change, which was pushed in the
    past from another working copy of the repo. Did not want
    what was actually changed to go out.
    
    This reverts commit 421fefdcc976f263693eb8fd86dbec2a2eb80452.

--- odum.sh
+++ odum.sh
@@ -36,9 +36,14 @@ LICENSE
 
 There are two situations where this script will fail to report properly:
 
-1. When traffic reaches a point that the byte counter of netstat rolls over to zero
+1. When traffic reaches a point that the byte counter of netstat rolls over to zero.
+   This can happen if the system is not rebooted for a period of time, although it 
+   is unlikely to occur on a home connection, especially if the system is keeps up
+   with the six month release cycle
 
-2. When the system is shutdown mid-month after it has started logging data
+2. When the system is shutdown mid-month after it has started logging data. The data
+   will continue to log and this can be computed manually if needed. It will start
+   working again once the next month starts.
 
 DISCLAIMER
 
